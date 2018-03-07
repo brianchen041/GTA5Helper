1e7e42094976b3c0b70267e3c7c15ff50e427ddd
Coldest Chen <brian824100@gmail.com>
2018-03-07 23:46:45 +0800

    Enable the UI

--- GTA5Helper/Form1.Designer.cs
+++ GTA5Helper/Form1.Designer.cs
@@ -28,12 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelState = new System.Windows.Forms.Label();
+            this.log = new System.Windows.Forms.TextBox();
+            this.domainUpDown = new System.Windows.Forms.DomainUpDown();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Run:";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStart.ForeColor = System.Drawing.Color.Blue;
+            this.buttonStart.Location = new System.Drawing.Point(244, 13);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 46);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(148, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "times";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 39);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "State:";
+            // 
+            // labelState
+            // 
+            this.labelState.AutoSize = true;
+            this.labelState.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelState.Location = new System.Drawing.Point(108, 78);
+            this.labelState.Name = "labelState";
+            this.labelState.Size = new System.Drawing.Size(88, 39);
+            this.labelState.TabIndex = 1;
+            this.labelState.Text = "None";
+            // 
+            // log
+            // 
+            this.log.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.log.Location = new System.Drawing.Point(24, 133);
+            this.log.Multiline = true;
+            this.log.Name = "log";
+            this.log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.log.Size = new System.Drawing.Size(316, 288);
+            this.log.TabIndex = 3;
+            // 
+            // domainUpDown
+            // 
+            this.domainUpDown.CausesValidation = false;
+            this.domainUpDown.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.domainUpDown.Items.Add("9");
+            this.domainUpDown.Items.Add("8");
+            this.domainUpDown.Items.Add("7");
+            this.domainUpDown.Items.Add("6");
+            this.domainUpDown.Items.Add("5");
+            this.domainUpDown.Items.Add("4");
+            this.domainUpDown.Items.Add("3");
+            this.domainUpDown.Items.Add("2");
+            this.domainUpDown.Items.Add("1");
+            this.domainUpDown.Location = new System.Drawing.Point(100, 13);
+            this.domainUpDown.Name = "domainUpDown";
+            this.domainUpDown.Size = new System.Drawing.Size(42, 47);
+            this.domainUpDown.TabIndex = 4;
+            this.domainUpDown.TabStop = false;
+            this.domainUpDown.Text = "5";
+            this.domainUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.domainUpDown.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.domainUpDown_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(367, 433);
+            this.Controls.Add(this.domainUpDown);
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelState);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "GTA5 Helper Beta";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelState;
+        private System.Windows.Forms.TextBox log;
+        private System.Windows.Forms.DomainUpDown domainUpDown;
     }
 }
 
--- GTA5Helper/Form1.cs
+++ GTA5Helper/Form1.cs
@@ -1,20 +1,92 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GTA5Helper
 {
     public partial class Form1 : Form
-    {
+    {        
+        const int SWITCH_PROCESS_ID = 0;
+
+        Boolean mIsProcessRunning = false;
+        int mTotalRunningTimes;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        enum KeyModifier
+        {
+            None = 0,
+            Alt = 1,
+            Control = 2,
+            Shift = 4,
+            WinKey = 8
+        }
+
         public Form1()
         {
             InitializeComponent();
+            RegisterHotKey(this.Handle, SWITCH_PROCESS_ID, (int)KeyModifier.None, Keys.F6.GetHashCode());            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, SWITCH_PROCESS_ID);
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            switchProcessState();
+        }
+
+        private void switchProcessState()
+        {
+            if(mIsProcessRunning)            
+                stopProcess();            
+            else           
+                startProcess();            
+            mIsProcessRunning = !mIsProcessRunning;
+        }
+
+        private void startProcess()
+        {
+            // UI
+            buttonStart.Text = "Stop";
+            buttonStart.ForeColor = Color.Red;
+            domainUpDown.ReadOnly = true;
+            log.AppendText("Start the process with " + domainUpDown.Text + " times.\n");
+        }
+
+        private void stopProcess()
+        {
+            // UI
+            buttonStart.Text = "Start";
+            buttonStart.ForeColor = Color.Blue;
+            domainUpDown.ReadOnly = false;
+            log.AppendText("Stop the process.\n");
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == 0x0312)
+            {
+                switch (m.WParam.ToInt32())
+                {
+                    case SWITCH_PROCESS_ID:
+                        switchProcessState();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void domainUpDown_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
         }
     }
 }
