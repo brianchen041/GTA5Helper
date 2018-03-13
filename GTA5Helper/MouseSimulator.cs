104b8c1bedf5b10bc859b3dff84ea8a1d22f882b
Coldest Chen <brian824100@gmail.com>
2018-03-13 23:31:54 +0800

    implement MouseSimulator

---
+++ GTA5Helper/MouseSimulator.cs
@@ -0,0 +1,59 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace GTA5Helper
+{
+    class MouseSimulator
+    {
+        [DllImport("user32.dll")]
+        private static extern bool SetCursorPos(int X, int Y);
+
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, int dwExtraInfo);
+
+        private static readonly int WIDTH = Screen.PrimaryScreen.Bounds.Width;
+        private static readonly int HEIGHT = Screen.PrimaryScreen.Bounds.Height;
+        private static readonly double SCALE_MUTIPLE_WIDTH = (double)65535 / (WIDTH-1);
+        private static readonly double SCALE_MUTIPLE_HEIGHT = (double)65535 / (HEIGHT-1);
+
+        enum MouseEventFlags
+        {
+            LEFTDOWN = 0x0002,
+            LEFTUP = 0x0004,
+            MIDDLEDOWN = 0x0020,
+            MIDDLEUP = 0x0040,
+            MOVE = 0x0001,
+            ABSOLUTE = 0x8000,
+            RIGHTDOWN = 0x0008,
+            RIGHTUP = 0x0010,            
+        }
+
+        public static void LeftClick(int x, int y)
+        {
+            uint scaleX = (uint)(x * SCALE_MUTIPLE_WIDTH);
+            uint scaleY = (uint)(y * SCALE_MUTIPLE_HEIGHT);
+            mouse_event((uint)(MouseEventFlags.LEFTDOWN | MouseEventFlags.ABSOLUTE), scaleX, scaleY, 0, 0);
+            mouse_event((uint)(MouseEventFlags.LEFTUP | MouseEventFlags.ABSOLUTE), scaleX, scaleY, 0, 0);
+        }
+
+        public static void RightClick(int x, int y)
+        {
+            uint scaleX = (uint)(x * SCALE_MUTIPLE_WIDTH);
+            uint scaleY = (uint)(y * SCALE_MUTIPLE_HEIGHT);
+            mouse_event((uint)(MouseEventFlags.RIGHTDOWN | MouseEventFlags.ABSOLUTE), scaleX, scaleY, 0, 0);
+            mouse_event((uint)(MouseEventFlags.RIGHTUP | MouseEventFlags.ABSOLUTE), scaleX, scaleY, 0, 0);
+        }
+
+        public static void MoveTo(int x, int y)
+        {
+            uint scaleX = (uint)(x * SCALE_MUTIPLE_WIDTH);
+            uint scaleY = (uint)(y * SCALE_MUTIPLE_HEIGHT);
+            mouse_event((uint)(MouseEventFlags.MOVE | MouseEventFlags.ABSOLUTE), scaleX, scaleY, 0, 0);            
+        }
+
+        public static void TeleportTo(int x, int y)
+        {
+            SetCursorPos(x, y);
+        }
+    }
+}
--- GTA5Helper/MyTask.cs
+++ GTA5Helper/MyTask.cs
@@ -6,28 +6,6 @@ namespace GTA5Helper
 {
     class MyTask
     {
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
-        static extern bool SetCursorPos(int X, int Y);
-
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
-        static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, int dwExtraInfo);
-
-        enum MouseEventFlags
-        {
-            LEFTDOWN = 0x00000002,
-            LEFTUP = 0x00000004,
-            MIDDLEDOWN = 0x00000020,
-            MIDDLEUP = 0x00000040,
-            MOVE = 0x00000001,
-            ABSOLUTE = 0x00008000,
-            RIGHTDOWN = 0x00000008,
-            RIGHTUP = 0x00000010,
-            MOWSEEVENTF_ABSOLOTE = 0x8000
-        }
-
-        const uint ABS_MOVE_FLAG = (uint)(MouseEventFlags.MOVE | MouseEventFlags.MOWSEEVENTF_ABSOLOTE);
-
-
         public static Boolean sIsRun = false;
 
         const int SECONDS_ONE = 1 * 1000;
@@ -43,13 +21,10 @@ namespace GTA5Helper
 
         private static void preventIdle()
         {
-            // TODO: Find a better way.
-            int x = 65536 / 2;            
-            int y = 65536 / 2;
-            SetCursorPos(960, 540);
-            mouse_event(ABS_MOVE_FLAG, (uint)x, (uint)(y-66), 0, 0);            
+            MouseSimulator.TeleportTo(960, 540);
+            MouseSimulator.MoveTo(960, (540 - 1));            
             Thread.Sleep(SECONDS_ONE);
-            mouse_event(ABS_MOVE_FLAG, (uint)x, (uint)y, 0, 0);
+            MouseSimulator.MoveTo(960, 540);
             Thread.Sleep(SECONDS_TEN);
         }
     }    
